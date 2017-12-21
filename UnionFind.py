51fc0053f7a14f34907ab367a8704510e0b72e62
Donghan Lee <Donghan.Lee@kofax.com>
2017-12-20 16:20:39 -0800

    Update UnionFind

---
+++ UnionFind.py
@@ -0,0 +1,36 @@
+# Union-find data structure.
+
+class UnionFind:
+    def __init__(self):
+        self.weights = {}
+        self.parents = {}
+    
+    def __getitem__(self, object):
+        # Find and return the name of the set containing the object
+        if object not in self.parents:
+            self.parents[object] = object
+            self.weights[object] = 1
+            return object
+        # find path of objects leading to the root
+        path = [object]
+        root = self.parents[object]
+        while root != path[-1]:
+            path.append(root)
+            root = self.parents[root]
+        
+        # compress the path and return
+        for ancestor in path:
+            self.parents[ancestor] = root
+        return root
+
+    def __iter__(self):
+        # Iterate through all items ever found or unioned by this structure
+        return iter(self.parents)
+
+    def union(self, *objects):
+        roots = [self[x] for x in objects]
+        heaviest = max([(self.weights[r], r) for r in roots])[1]
+        for r in roots:
+            if r != heaviest:
+                self.weights[heaviest] += self.weights[r]
+                self.parents[r] = heaviest
